6c960893eaeb255ef5466b69222eb7bd5787fd7f
Guilherme Silva <guilherme.silva@codeblox.com.br>
2020-12-10 10:42:34 -0300

    Adicionando comparação de senha

--- ProAgil-App/src/app/user/registration/registration.component.ts
+++ ProAgil-App/src/app/user/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -17,16 +17,27 @@ export class RegistrationComponent implements OnInit {
     this.validation();
   }
 
-  validation(){
+  validation() {
     this.registerForm = this.fb.group({
-        fullName : ['', Validators.required],
-        email : ['', Validators.required, Validators.email],
-        userName : ['', Validators.required],
-        passwords : this.fb.group({
-          password : ['', Validators.required, Validators.minLength(4)],
-          confirmPassword : ['', Validators.required]
-        })
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      passwords: this.fb.group({
+        password: ['', [Validators.required, Validators.minLength(4)]],
+        confirmPassword: ['', Validators.required]
+      }, { validator: this.compararSenhas })
     });
+  }
+
+  compararSenhas(fb: FormGroup) {
+    const confirmSenhaCtrl = fb.get('confirmPassword');
+    if (confirmSenhaCtrl.errors == null || 'mismatch' in confirmSenhaCtrl.errors) {
+      if (fb.get('password').value !== confirmSenhaCtrl.value) {
+        confirmSenhaCtrl.setErrors({ mismatch: true });
+      } else {
+        confirmSenhaCtrl.setErrors(null);
+      }
+    }
   }
 
   cadastrarUsuario(){
